fcb007ba4cc213036c09c13226398a80e1b6a7dd
vansak <vansakwebdevelopment@gmail.com>
2024-02-05 05:00:31 +0700

    fix search word list

--- app/dictionary/[id]/layout.tsx
+++ app/dictionary/[id]/layout.tsx
@@ -1,3 +1,31 @@
+import { hsk } from "@/lib/data";
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const word: Hsk = await hsk.one(Number(params.id));
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: `${word?.chinese} ${word?.chinese && "|"}
+    ${word?.pinyin[0]} ${word?.pinyin[0] && "|"}
+    ${word?.khmer[0]} ${word?.khmer[0] && "|"}
+    ${word?.english[0]}`,
+    // description: `${word?.chinese}:${word?.pinyin} \n ${word?.khmer} \n ${word?.english} `,
+    openGraph: {
+      images: ["/favicon.png", ...previousImages],
+    },
+  };
+}
+
+
+
 export default async function DictionaryLayout({
   children,
 }: {
--- app/dictionary/[id]/page.tsx
+++ app/dictionary/[id]/page.tsx
@@ -1,27 +1,4 @@
-import type { Metadata, ResolvingMetadata } from "next";
 
-type Props = {
-  params: { id: string };
-};
-
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const word: Hsk = await hsk.one(Number(params.id));
-  const previousImages = (await parent).openGraph?.images || [];
-
-  return {
-    title: `${word?.chinese} ${word?.chinese && "|"}
-    ${word?.pinyin[0]} ${word?.pinyin[0] && "|"}
-    ${word?.khmer[0]} ${word?.khmer[0] && "|"}
-    ${word?.english[0]}`,
-    description: `${word?.chinese}:${word?.pinyin} \n ${word?.khmer} \n ${word?.english} `,
-    openGraph: {
-      images: ["/favicon.png", ...previousImages],
-    },
-  };
-}
 
 import { hsk } from "@/lib/data";
 import { sentence } from "@/lib/data";
